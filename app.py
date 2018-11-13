1b13853a80d599ed284765ec714d5bb8b3889c9f
Victor Dahlberg <victor.dahlberg@capgemini.com>
2018-11-13 13:14:25 +0100

    df.show

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ connectionProperties = {
 
 vehicle_type_table = spark.read.jdbc(url=jdbcUrl, table="trafikkdata", properties=connectionProperties)
 test=vehicle_type_table.select('vehicle_type_quality', 'vehicle_type').groupBy('vehicle_type').avg('vehicle_type_quality')
-display(test)
+test.show()
 
 spark.stop()
 
